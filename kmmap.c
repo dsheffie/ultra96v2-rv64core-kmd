098682e686848c74396969e38a550993df45c9ec
David Sheffield <sheffield.david@gmail.com>
2025-10-18 15:39:28 +0000

    more plumbing to pass address through ioctl

--- kmmap.c
+++ kmmap.c
@@ -10,6 +10,9 @@
 static struct device* dma_dev = NULL;
 static dma_addr_t dma_handle;
 static void* cpu_addr = NULL;
+
+static int Device_Open = 0;
+
 #define CMA_MALLOC_DEVICE_FILENAME "rv64core_fpga"
 #define REGION_SZ (448*1024*1024)
 
@@ -18,11 +21,27 @@ static int cma_malloc_mmap(struct file* fptr, struct vm_area_struct* vma){
 };
 
 static long cma_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+  printk(KERN_INFO "someone attempting to do an ioctl\n");
+  return 0;
+}
+
+static int cma_open(struct inode *inode_, struct file * file_) {
+  if(Device_Open) {
+    return -EBUSY;
+  }
+  Device_Open++;
+  return 0;
+}
+
+static int cma_release(struct inode *inode_, struct file * file_) {
+  Device_Open--;
   return 0;
 }
 
 static struct file_operations cma_malloc_fileops = {
     .owner            =   THIS_MODULE,
+    .open             =   cma_open,
+    .release          =   cma_release,
     .mmap             =   cma_malloc_mmap,
     .unlocked_ioctl   =   cma_ioctl
 };
@@ -37,7 +56,6 @@ static struct miscdevice cma_malloc_miscdevice = {
 
 static int __init fpga_init(void) {
   int ret;
-  size_t i;
   ret = misc_register(&cma_malloc_miscdevice);
   if(ret) {
     return -EIO;
@@ -51,9 +69,8 @@ static int __init fpga_init(void) {
     return -ENOMEM;
 
   printk(KERN_NOTICE "linux virtual address = %lx, phys addr = %lx\n", cpu_addr, dma_handle);
-  for(i = 0; i < ((REGION_SZ)/sizeof(int)); i++) {
-    ((int*)cpu_addr)[i] = i;
-  }
+
+
   return 0; // success
 }
 
